c2e901d3fa1efef7caafde23116a7083678c503e
pachet <pachet@burninggarden.com>
2013-07-25 00:35:39 -0500

    Adding a test adapter and binding for the promises/a+ test suite.

---
+++ test/aplus-adapter.js
@@ -0,0 +1,17 @@
+var
+	promix = require('../index');
+
+function pending ( ) {
+	var
+		promise = promix.promise();
+
+	return {
+		promise : promise,
+		fulfill : promise.fulfill,
+		reject : promise.reject
+	};
+}
+
+module.exports = {
+	pending : pending
+};
