d4cbdd69d83466dfd3ebf394a1dcdb51f387d674
pachet <pachet@burninggarden.com>
2013-08-25 10:07:13 -0500

    Changing everything back to camelCase.

--- index.js
+++ index.js
@@ -3,23 +3,73 @@ var
 	Promise = require('./lib/promise'),
 	handler = require('./lib/handler'),
 	Exposure = require('./lib/exposure'),
-	types = require('./lib/types');
+	types = require('./lib/types'),
+	utils = require('./lib/utils');
 
-function when ( ) {
-	var
-		result = new Exposure(),
-		args = Array.prototype.slice.call(arguments);
 
-	if ( args.length ) {
-		result.and.apply(result, args);
-	}
-	return result;
+module.exports = {
+	promise : Promise,
+	toString : types.toString,
+	toNumber : types.toNumber,
+	toArray : types.toArray,
+	toObject : types.toObject,
+	handle : handler.set,
+	when : when,
+	chain : when,
+	join : join,
+	fork : fork,
+	invoke : invoke,
+	concat : concat,
+	wrap : wrap,
+	forward : wrap,
+	errorless : errorless,
+	succeed : succeed,
+	next : succeed,
+	fail : fail,
+	compose : compose,
+	version : require('./package.json').version
+};
+
+// expose the module at `window.promix` if we're in a browser:
+if ( typeof window !== 'undefined' && typeof window.promix === 'undefined' ) {
+	window.promix = module.exports;
 }
 
-function join ( failure, success ) {
-	return function apply_fork ( ) {
+
+// Takes an optional promise or fn to invoke, and returns a new chain.
+//
+// Usage :
+//	promix.when()
+//	promix.when(<thenable> promise)
+//	promix.when(<function> callback [, arg1, arg2, ...])
+//
+// Returns :
+//	<Chain>
+//
+function when ( ) {
+	var
+		chain = new Exposure();
+
+	// only call chain.and() if they actually supplied arguments:
+	if ( arguments.length ) {
+		chain.and.apply(chain, utils.copy(arguments));
+	}
+	return chain;
+}
+
+// Takes discrete success and failure callbacks,
+// and turns them into a consolidated error-first callback.
+//
+// Usage :
+//	promix.join(<function> successFn, <function> failureFn)
+//
+// Returns :
+//	<function>
+//
+function join ( success, failure ) {
+	return function applyJoin ( ) {
 		var
-			args = Array.prototype.slice.call(arguments),
+			args = utils.copy(arguments),
 			error = args.shift();
 
 		if ( error ) {
@@ -29,18 +79,39 @@ function join ( failure, success ) {
 	};
 }
 
+// Applies a standard error-first callback to a promise.
+//
+// Usage :
+//	promix.fork(<object> promise, <function> callback)
+//
 function fork ( promise, callback ) {
 	promise.then(function abstracted_callback ( result ) {
 		return callback(null, result);
 	}, callback);
 }
 
+// Turns a success-only callback into a standard error-first callback.
+//
+// Usage :
+//	promix.errorless(<function> callback)
+//
+// Returns :
+//	<function>
+//
 function errorless ( callback ) {
 	return function errorless ( ) {
-		return callback.apply(null, [null].concat(Array.prototype.slice.call(arguments)));
+		return callback.apply(null, [null].concat(utils.copy(arguments)));
 	};
 }
 
+// Transforms a promise into a standard callback.
+//
+// Usage :
+//	promix.wrap(<object> promise, <?> value)
+//
+// Returns :
+//	<function>
+//
 function wrap ( promise, value ) {
 	return function ( error, result ) {
 		if ( error ) {
@@ -50,10 +121,21 @@ function wrap ( promise, value ) {
 	};
 }
 
+// Invokes the supplied callback-accepting function,
+// passes in any arguments + a synthetic callback,
+// and returns a promise that will be fulfilled/rejected
+// when that callback is called.
+//
+// Usage :
+//	promix.invoke(<function> callbackAcceptingFn [, arg1, arg2, ...])
+//
+// Returns :
+//	<Promise>
+//
 function invoke ( ) {
 	var
 		promise,
-		args = Array.prototype.slice.call(arguments),
+		args = utils.copy(arguments),
 		primary;
 
 	if ( typeof args [0] !== 'function' ) {
@@ -71,16 +153,31 @@ function invoke ( ) {
 	return promise;
 }
 
+// Returns a promise that will be fulfilled at the next tick/breath.
+//
+// Usage :
+//	promix.succeed(<?> value)
+//
+// Returns :
+//	<Promise>
 function succeed ( value ) {
 	var
 		promise = Promise();
 
-	setTimeout(function ( ) {
+	utils.next(function wrapper ( ) {
 		return void promise.resolve(value);
-	}, 0);
+	});
 	return promise;
 }
 
+// Returns a promise that will be rejected at the next tick/breath.
+//
+// Usage :
+//	promix.fail(<?> reason)
+//
+// Returns :
+//	<Promise>
+//
 function fail ( reason ) {
 	var
 		promise = Promise();
@@ -88,12 +185,21 @@ function fail ( reason ) {
 	if ( ! ( reason instanceof Error ) ) {
 		reason = new Error(reason);
 	}
-	setTimeout(function ( ) {
+	utils.next(function wrapper ( ) {
 		return void promise.reject(reason);
-	}, 0);
+	});
 	return promise;
 }
 
+// Returns a new Chain with a discrete step for each property in the
+// supplied object. This lets us do cool things like:
+//
+// promix.compose({
+//	foo : fooPromise,
+//	bar : 2,
+//	baz : bazPromise
+// }).then(fn);
+//
 function compose ( object ) {
 	var
 		chain = new Exposure();
@@ -104,9 +210,16 @@ function compose ( object ) {
 	return chain;
 }
 
+// Convenience wrapper to concatenate promises into a single StringPromise.
+//
+// Usage :
+//	promix.concat(<string || Promise> arg1 [, arg2, arg3, arg4])
+//
+// Returns :
+//	<StringPromise>
 function concat ( ) {
 	var
-		args = Array.prototype.slice.call(arguments),
+		args = utils.copy(arguments),
 		primary = args.shift(),
 		intermediary;
 
@@ -114,21 +227,3 @@ function concat ( ) {
 	return intermediary.concat.apply(intermediary, args);
 }
 
-module.exports = types;
-module.exports.handle = handler.set;
-module.exports.promise = Promise;
-module.exports.when = module.exports.chain = when;
-module.exports.join = join;
-module.exports.fork = fork;
-module.exports.invoke = invoke;
-module.exports.concat = concat;
-module.exports.wrap = module.exports.forward = wrap;
-module.exports.errorless = errorless;
-module.exports.succeed = module.exports.next = succeed;
-module.exports.fail = fail;
-module.exports.compose = compose;
-module.exports.version = require('./package.json').version;
-
-if ( typeof window !== 'undefined' && typeof window.promix === 'undefined' ) {
-	window.promix = module.exports;
-}
